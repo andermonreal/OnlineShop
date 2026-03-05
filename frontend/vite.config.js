da483556f9eeb48e47d782df8f3aae8793b73dfa
ander <andermonreal2003@gmail.com>
2026-03-05 19:23:16 +0100

    Automatic deploy script

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: process.env.FRONTEND_PORT || 80,
     proxy: {
       '/onlineShop': {
-        target: 'http://localhost:8080',
+        target: process.env.VITE_API_URL || 'http://localhost:8080',
         changeOrigin: true,
       }
     }
